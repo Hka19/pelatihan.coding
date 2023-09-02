e43bf5be1f6f6ce0d406a8ce80436e713469a7c2
Hka19 <143859108+Hka19@users.noreply.github.com>
2023-09-02 10:44:52 +0700

    Add files via upload

---
+++ coding.py
@@ -0,0 +1,31 @@
+apel = 52
+print(apel)
+print(type(521))
+apel2 =52
+# print
+#  nama_apel =
+#  a1pel =
+apel_benar = True
+apel_salah = False
+print(type(apel_benar))
+print(apel_salah)
+
+list_warna_apel = ['hijau','merah']
+list_apel1 = ["manis", 2 ,3.0]
+print(list_warna_apel)
+print(list_apel1)
+
+tupel_jumlah = (2,5.25,"satu")
+print(tupel_jumlah)
+
+
+print(34+2)
+print(39-4)
+print(2*6)
+print(9/7)
+
+e = 50
+c = 10
+print (e*c)
+
+print(Fahrizal)
